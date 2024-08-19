28a4d8ee5a734269430eb30bf8d3f8dedc211445
felix <1393491999@qq.com>
2024-08-19 16:10:54 +0800

    refactor: 优化babel配置

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,22 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        corejs: 3,
-        useBuiltIns: 'usage' // 按需导入
+        modules: 'auto'
       }
     ],
     '@babel/preset-react',
     '@babel/preset-typescript'
   ],
   plugins: [
-    '@babel/plugin-transform-runtime',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 3,
+        helpers: true,
+        regenerator: true,
+        useESModules: false
+      }
+    ],
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import'
   ]
